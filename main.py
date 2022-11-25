105f7e6255a060e06c6c302552d827032d3f13fb
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-11-25 11:18:33 -0700

    started program

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+title: Large Mammal Population Calculator
+author: Michelle Jiang
+date-created: 2022-11-25
+"""
+
+# Most of the time will be spent manipulating the data
+# Must manipulate the data with code, not editing the txt file
+# for add new year data, should have user be able to input data for every column in the database 
+# only need to query the database and add new columns, don't need to edit data 
+# commas exist in comments so you need to edit the data before splitting by commas 
+# don't actually need to normalize 
