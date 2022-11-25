04f30c68c4d452e483fa80131223a496d57e76af
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-11-25 11:26:17 -0700

    formatting

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ date-created: 2022-11-25
 # only need to query the database and add new columns, don't need to edit data 
 # commas exist in comments so you need to edit the data before splitting by commas 
 # don't actually need to normalize 
+
+### VARIABLES ### 
+
+DATABASENAME = "Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.csv"
+
+
+### FUNCTIONS ### 
+
+# INPUTS # 
+
+# PROCESSING # 
+
+# OUTPUTS #
+
+### MAIN PROGRAM CODE ###
+
+# INPUTS #
+
+# PROCESSING # 
+
+# OUPUTS # 
