6850db2789878ac7a50debf71df87aeb46a9476f
Michelle Jiang <m.jiang4@share.epsb.ca>
2022-11-28 13:47:50 -0700

    tryna remember regex

--- main.py
+++ main.py
@@ -12,22 +12,64 @@ date-created: 2022-11-25
 # don't actually need to normalize 
 
 ### VARIABLES ### 
+import sqlite3
+import pathlib
+import re # regular expressions
+DATABASEFILE = "large mammal population.db"
 
-DATABASENAME = "Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.csv"
+FIRSTRUN = True 
+if (pathlib.Path.cwd() / DATABASEFILE).exists():
+    FIRSTRUN = False
 
-
+CONNECTION = sqlite3.connect(DATABASEFILE)
+CURSOR = CONNECTION.cursor()
 ### FUNCTIONS ### 
 
 # INPUTS # 
 
+def extractFile(FILENAME) -> list:
+    """
+    extract file content and puts it into the database 
+    :param FILENAME: str
+    :return: list
+    """
+
+    # get file content
+    FILE = open(FILENAME)
+    TEXTLIST = FILE.readlines()
+    FILE = FILE.close()
+
+    # clean data 
+    for i in range(len(TEXTLIST)):
+        if TEXTLIST[i][-1] == "\n":
+            TEXTLIST[i] = TEXTLIST[i][:-1] 
+        TEXTLIST[i] = TEXTLIST[i].re.split(r',(?=")') # read docs
+        for j in range(len(TEXTLIST[i])):
+            if TEXTLIST[i][j].isnumeric():
+                TEXTLIST[i][j] = int(TEXTLIST[i][j])
+            if TEXTLIST[i][j] == "NA" or TEXTLIST[i][j] == "":
+                TEXTLIST[i][j] = 0
+    
+    return TEXTLIST
+
 # PROCESSING # 
+
+def setup(CONTENT) -> None:
+    """
+    creates the database file on first run 
+    :param CONTENT: list
+    :return: None
+    """
 
 # OUTPUTS #
 
 ### MAIN PROGRAM CODE ###
-
+if __name__ == "__main__":
 # INPUTS #
-
+    if FIRSTRUN:
+        CONTENT = extractFile("Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.csv")
+        print(CONTENT)
+        setup(CONTENT)
 # PROCESSING # 
 
 # OUPUTS # 
